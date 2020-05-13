86b911fd16adbdf201157fd46cda405db015cb73
jzhu379 <jzhu379@gmail.com>
2020-05-12 17:39:35 -0700

    deleted unnecessary audio files

--- src/images/index.js
+++ src/images/index.js
@@ -6,8 +6,4 @@ export { default as MelonJPG } from './fruit/melon.jpg';
 export { default as MelonActiveJPG } from './fruit/melon_active.jpg';
 export { default as OrangeJPG } from './fruit/orange.jpg';
 export { default as OrangeActiveJPG } from './fruit/orange_active.jpg';
-export { default as SkyBotJPG } from './skybox/sky_bot.jpg';
-export { default as SkyTopJPG } from './skybox/sky_top.jpg';
-export { default as Sky1JPG } from './skybox/sky1.jpg';
-export { default as Sky2JPG } from './skybox/sky2.jpg';
 export { default as StarJPG } from './powerups/star.jpg';
